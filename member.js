778458f4b5b0b8ab3171382440c75d21b7603a4f
Eder-Sousa <ederdi395@gmail.com>
2024-03-01 02:11:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+  return {
+    members: [
+      { name: 'John', skills: ['JavaScript', 'React'] },
+      { name: 'Jane', skills: ['JavaScript', 'Angular'] },
+      { name: 'Tom', skills: ['JavaScript', 'React'] },
+    ],
+  };
+}
